a44c2c2b76c8be262aa5a1b8b173b1907e2ab75f
Robin Verachtert <robinverachtert@Robins-MacBook-Pro.local>
2023-02-03 09:39:54 +0100

    Added options for scenarios

--- runall_amazon_toys_and_games.sh
+++ runall_amazon_toys_and_games.sh
@@ -1,7 +1,13 @@
 #! /bin/bash
 
-python3 run.py --dataset amazon_toys_and_games --algorithm Popularity --timeout 3600
-python3 run.py --dataset amazon_toys_and_games --algorithm ItemKNN --timeout 7200
-python3 run.py --dataset amazon_toys_and_games --algorithm EASE --timeout 7200
-python3 run.py --dataset amazon_toys_and_games --algorithm SequentialRules --timeout 7200
-python3 run.py --dataset amazon_toys_and_games --algorithm GRU4RecNegSampling --timeout 21600
+python3 run.py --dataset amazon_toys_and_games --algorithm Popularity --timeout 3600 --scenario-name Timed
+python3 run.py --dataset amazon_toys_and_games --algorithm ItemKNN --timeout 7200 --scenario-name Timed
+python3 run.py --dataset amazon_toys_and_games --algorithm EASE --timeout 7200 --scenario-name Timed
+python3 run.py --dataset amazon_toys_and_games --algorithm SequentialRules --timeout 7200 --scenario-name Timed
+python3 run.py --dataset amazon_toys_and_games --algorithm GRU4RecNegSampling --timeout 21600 --scenario-name Timed
+
+# python3 run.py --dataset amazon_toys_and_games --algorithm Popularity --timeout 3600 --scenario-name TimedLastItemPrediction
+# python3 run.py --dataset amazon_toys_and_games --algorithm ItemKNN --timeout 7200 --scenario-name TimedLastItemPrediction
+# python3 run.py --dataset amazon_toys_and_games --algorithm EASE --timeout 7200 --scenario-name TimedLastItemPrediction
+# python3 run.py --dataset amazon_toys_and_games --algorithm SequentialRules --timeout 7200 --scenario-name TimedLastItemPrediction
+# python3 run.py --dataset amazon_toys_and_games --algorithm GRU4RecNegSampling --timeout 21600 --scenario-name TimedLastItemPrediction
